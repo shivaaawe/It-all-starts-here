a05b5e963618264292cfba2d7d3454ff003566cc
shivaaawe <mikkilishiva@gmail.com>
2024-11-23 02:16:53 +0530

    Changes to be committed:

---
+++ main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args) {
+        int firstNumber = 0;
+        int secondNumber = 1;
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i=1;i<=num;i++){
+            System.out.println(firstNumber);
+            int nextNumber = firstNumber+secondNumber;
+            firstNumber=secondNumber;
+            secondNumber=nextNumber;
+        }
+    }
+
+}
