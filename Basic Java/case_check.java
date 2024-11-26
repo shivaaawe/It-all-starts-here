c72173b6d6f8e31f1d08ad770d212e1e6647424c
shivaaawe <mikkilishiva@gmail.com>
2024-11-27 01:02:24 +0530

    modified:   case_check.java

--- Basic Java/case_check.java
+++ Basic Java/case_check.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class case_check {
     public static void main(String[] args) {
-        System.out.println("Case Checker!");
+        System.out.println("Case Checker");
         Scanner sc = new Scanner(System.in);
         char ch = sc.next().trim().charAt(0);
         if (ch >= 'a' && ch <= 'z'){
