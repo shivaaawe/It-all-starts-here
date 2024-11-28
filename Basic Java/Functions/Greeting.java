778ef64814ebaaf88e9a5f23f540ec210bbfb010
shivaaawe <mikkilishiva@gmail.com>
2024-11-29 00:30:09 +0530

    new file:   Functions/Greeting.java

---
+++ Basic Java/Functions/Greeting.java
@@ -0,0 +1,10 @@
+package Functions;
+public class Greeting{
+    public static void main(String[] args) {
+        greeting();
+        greeting();
+    }
+    static void greeting(){
+        System.out.println("Hello World");
+    }
+}
