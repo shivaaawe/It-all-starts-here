5e18a3aebf65bde4e17c229467147b3d411fbc88
shivaaawe <mikkilishiva@gmail.com>
2024-11-29 00:27:40 +0530

    Starting off with the Functions.

---
+++ Basic Java/Functions/basic.java
@@ -0,0 +1,10 @@
+package Functions;
+public class basic{
+    public static void main(String[] args) {
+        greeting();
+        greeting();
+    }
+    static void greeting(){
+        System.out.println("Hello World");
+    }
+}
