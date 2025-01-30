5f80820c24787242aa4f517e297267048207f9af
Mikkili Shiva Sai Reddy <91215905+shivaaawe@users.noreply.github.com>
2025-01-30 11:36:52 +0530

    updated

--- Basic Java/Patterns/nTriangle.java
+++ Basic Java/Patterns/nTriangle.java
@@ -1,6 +1,6 @@
 public class nTriangle{
     public static void npattern2(int n){
-        for (int i = 1; i < n; i++){
+        for (int i = 1; i <= n; i++){
             for (int j = 1; j <= i; j++){
                 System.out.print(j+" ");
             }
