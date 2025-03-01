483b92a71835a7f8516604de013502a7bdbbd248
Mikkili Shiva Sai Reddy <91215905+shivaaawe@users.noreply.github.com>
2025-03-01 20:52:21 +0530

    Prime or not using Functions

---
+++ AC/Functions/primeornot.java
@@ -0,0 +1,18 @@
+package AC.Functions;
+
+public class primeornot {
+    public static boolean primeornot1(int n){
+        if (n==2){
+            return true;
+        }
+        for(int i=2; i<= n-1; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+      System.out.println(primeornot1(13));  
+    }
+}
