d1aebcd208a69a140c715af8076edb24bf2fcd06
MIKKILI SHIVA SAI REDDY <91215905+shivaaawe@users.noreply.github.com>
2025-03-03 14:58:46 +0530

    Create palindrome_or_not.java

---
+++ Leetcode/palindrome_or_not.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static boolean isPalindrome(int x) {  
+        int myNum = x;
+        int rev = 0;
+        
+        if (x < 0) return false; // 
+        
+        while (x > 0) {
+            int lastDigit = x % 10;
+            rev = (rev * 10) + lastDigit;
+            x /= 10;
+        }
+        
+        return myNum == rev; 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121)); 
+    }
+}
