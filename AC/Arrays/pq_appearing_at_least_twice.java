0aee89669158d31aec77552c7e8cd77acb5d6f56
Mikkili Shiva Sai Reddy <91215905+shivaaawe@users.noreply.github.com>
2025-03-17 13:43:47 +0530

    .\pq_appearing_at_least_twice.java - bruteforce approach

---
+++ AC/Arrays/pq_appearing_at_least_twice.java
@@ -0,0 +1,18 @@
+package AC.Arrays;
+
+public class pq_appearing_at_least_twice {
+    public static boolean appearing_at_least_twice(int [] numbers){
+        for(int i=0; i<numbers.length-1; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                if(numbers[i] == numbers[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int numbers [] = {1,2,3,4,6};
+        System.out.println(appearing_at_least_twice(numbers));
+    }
+}
