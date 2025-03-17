8a8cbf908f9239602f9f4bbe263fc3eaee011269
Mikkili Shiva Sai Reddy <91215905+shivaaawe@users.noreply.github.com>
2025-03-17 13:21:01 +0530

    .\buy_and_sell_stocks.java

---
+++ AC/Arrays/buy_and_sell_stocks.java
@@ -0,0 +1,22 @@
+package AC.Arrays;
+
+public class buy_and_sell_stocks {
+    public static int buyandsellstocks(int [] prices){
+        int buyingPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0; i<prices.length; i++){
+            if(buyingPrice<prices[i]){ // profit
+                int profit = prices[i] - buyingPrice; //today's profit
+                maxProfit = Math.max(maxProfit, profit);
+            }else{
+                buyingPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int prices [] = {7,1,5,3,6,4};
+        System.out.println(buyandsellstocks(prices));
+    }
+}
