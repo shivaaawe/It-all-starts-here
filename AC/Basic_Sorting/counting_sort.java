8dee8d2d2f9590d9001f973a51a551407d21a506
Mikkili Shiva Sai Reddy <91215905+shivaaawe@users.noreply.github.com>
2025-03-20 03:38:16 +0530

    .\counting_sort.java

---
+++ AC/Basic_Sorting/counting_sort.java
@@ -0,0 +1,34 @@
+package AC.Basic_Sorting;
+
+public class counting_sort {
+    public static void countingsort(int [] arr){
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+        for(int i=0; i<arr.length; i++){
+            count[arr[i]]++;
+        }
+
+        int j=0;
+        for(int i=0; i<count.length; i++){
+            while(count[i] > 0){
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+    public static void printArray(int [] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int [] arr = {5,2,3,6,1,4,7};
+        countingsort(arr);
+        printArray(arr);
+    }
+}
